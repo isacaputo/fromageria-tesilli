246f610a4771acff7dd7e4198b1f9fcd3a58b6fe
isacaputo <isadora.caputo@gmail.com>
2025-08-19 16:00:30 +0200

    Refactor order.js to enhance code readability by simplifying the associate method and formatting

--- models/order.js
+++ models/order.js
@@ -1,29 +1,27 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
-      Order.belongsToMany(models.Product, { through: models.OrderHasProduct, foreignKey: 'order_id' });
+      Order.belongsToMany(models.Product, {
+        through: models.OrderHasProduct,
+        foreignKey: 'order_id',
+      });
     }
   }
-  Order.init({
-    date: DataTypes.DATE,
-    total_amount: DataTypes.FLOAT,
-    client_name: DataTypes.STRING,
-    client_email: DataTypes.STRING,
-    client_phone: DataTypes.STRING,
-    client_address: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Order',
-  });
+  Order.init(
+    {
+      date: DataTypes.DATE,
+      total_amount: DataTypes.FLOAT,
+      client_name: DataTypes.STRING,
+      client_email: DataTypes.STRING,
+      client_phone: DataTypes.STRING,
+      client_address: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Order',
+    }
+  );
   return Order;
 };
