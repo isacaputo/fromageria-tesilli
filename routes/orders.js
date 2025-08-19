c759bc9994d968013d4dae539e48b5f7f174b51a
isacaputo <isadora.caputo@gmail.com>
2025-08-19 16:00:43 +0200

    Refactor orders.js to improve code readability by standardizing quote styles and removing commented-out code

--- routes/orders.js
+++ routes/orders.js
@@ -1,13 +1,12 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const nodemailer = require("nodemailer");
-const { transporter } = require("../nodemailer/message_transporter");
-const models = require("../models");
-const { Op } = require("sequelize");
-var userShouldBeLoggedIn = require('../guards/userShouldBeLoggedIn')
+const nodemailer = require('nodemailer');
+const { transporter } = require('../nodemailer/message_transporter');
+const models = require('../models');
+const { Op } = require('sequelize');
+var userShouldBeLoggedIn = require('../guards/userShouldBeLoggedIn');
 
-/* GET from orders table */
-router.get("/", userShouldBeLoggedIn, async function (req, res, next) {
+router.get('/', userShouldBeLoggedIn, async function (req, res, next) {
   try {
     const result = await models.Order.findAll({
       include: models.Product,
@@ -19,37 +18,25 @@ router.get("/", userShouldBeLoggedIn, async function (req, res, next) {
   }
 });
 
-/* INSERT into orders table */
-router.post("/", async function (req, res, next) {
+router.post('/', async function (req, res, next) {
   const { clientName, clientEmail, clientPhone, clientAddress, items } =
     req.body;
 
   try {
     const productIds = items.map((value) => value.id);
 
-    // const productResponse = await db(
-    //   `SELECT id, product_whole_price, product_half_price FROM products WHERE id IN (${productIds.join(
-    //     ","
-    //   )});`
-    // );
-
     const productResponse = await models.Product.findAll({
-      attributes: [ 'id', 'product_whole_price', 'product_half_price'],
+      attributes: ['id', 'product_whole_price', 'product_half_price'],
       where: {
         id: {
-          [Op.in]: productIds // Op refers to the operators object provided by sequelize. Defines various comparison
-          //operators for querying the database. In this case we are checking whether the Ids in the column match any of the Ids in the 
-          //product Ids array. 
-        }
-      }
-    })
-   
+          [Op.in]: productIds,
+        },
+      },
+    });
 
-     const productResponseData = productResponse.map((product) => product.dataValues);
-    
-    
-
-
+    const productResponseData = productResponse.map(
+      (product) => product.dataValues
+    );
 
     let amount = 0;
     items.forEach((item) => {
@@ -64,45 +51,36 @@ router.post("/", async function (req, res, next) {
       }
     });
 
-
-    await models.Order.create(
-    {
+    await models.Order.create({
       total_amount: amount,
       client_name: clientName,
       client_email: clientEmail,
       client_phone: clientPhone,
-      client_address: clientAddress
-    })
-
-
-    const ordersResponse = await models.Order.findOne({
-      attributes:["id"],
-      order: [['date', 'DESC']]
+      client_address: clientAddress,
     });
 
+    const ordersResponse = await models.Order.findOne({
+      attributes: ['id'],
+      order: [['date', 'DESC']],
+    });
 
     const orderId = ordersResponse.dataValues.id;
 
-    
-
-    const insertItemsQuery = items.map((item) => 
-    models.OrderHasProduct.create({
-      order_id: orderId,
-      product_id: item.id,
-      size: item.size,
-      quantity: item.quantity,
-
-    }));
+    const insertItemsQuery = items.map((item) =>
+      models.OrderHasProduct.create({
+        order_id: orderId,
+        product_id: item.id,
+        size: item.size,
+        quantity: item.quantity,
+      })
+    );
 
     await Promise.all(insertItemsQuery);
 
-
-
-
     let message = {
-      from: "Fromageria Tesilli <isadora.caputo@gmail.com>",
+      from: 'Fromageria Tesilli <isadora.caputo@gmail.com>',
       to: `${clientName} <${clientEmail}>`,
-      subject: "Fromageria Tesilli: Recebemos o seu pedido!",
+      subject: 'Fromageria Tesilli: Recebemos o seu pedido!',
       html: `<h2><b>Olá ${clientName}!</b></h2>
       <p>Estamos muito felizes em receber o seu pedido. Para que possa acompanhar o seu andamento, o número é: <b>${orderId}</b></p>
       <p>Em breve, entraremos em contato para detalhamento do frete e entrega.</p>
